729c85187cfecee0f1011799e8a1cc26b55bef0d
btpjamal <joaothexkronax@hotmail.com>
2026-04-27 13:36:42 -0300

    atualizando classes services, para utilizaçao de DTOs

--- src/main/java/dev/jamal/projetotcc/Service/HobbyService.java
+++ src/main/java/dev/jamal/projetotcc/Service/HobbyService.java
@@ -1,6 +1,11 @@
 package dev.jamal.projetotcc.Service;
 
+import dev.jamal.projetotcc.DTO.Hobby.HobbyCreateRequestDTO;
+import dev.jamal.projetotcc.DTO.Hobby.HobbyResponseDTO;
 import dev.jamal.projetotcc.Entities.Hobby;
+import dev.jamal.projetotcc.Entities.HobbyCategory;
+import dev.jamal.projetotcc.Mapper.HobbyMapper;
+import dev.jamal.projetotcc.Repository.HobbyCategoryRepository;
 import dev.jamal.projetotcc.Repository.HobbyRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
@@ -14,16 +19,32 @@ import java.util.List;
 public class HobbyService {
 
     private final HobbyRepository hobbyRepository;
+    private final HobbyCategoryRepository categoryRepository;
+    private final HobbyMapper hobbyMapper;
 
-    public Hobby salvar(Hobby hobby) {
-        return hobbyRepository.save(hobby);
+    public HobbyResponseDTO cadastrar(HobbyCreateRequestDTO dto) {
+
+        HobbyCategory category = categoryRepository.findById(dto.getCategoryId())
+                .orElseThrow(() -> new RuntimeException("Categoria não encontrada."));
+
+        Hobby hobby = hobbyMapper.toEntity(dto, category);
+        Hobby salvo = hobbyRepository.save(hobby);
+
+        return hobbyMapper.toResponseDTO(salvo);
     }
 
-    public List<Hobby> listarTodos(){
-        return hobbyRepository.findAll();
+
+    @Transactional
+    public List<HobbyResponseDTO> listarTodos() {
+        return hobbyRepository.findAllWithCategory()
+                .stream()
+                .map(hobbyMapper::toResponseDTO)
+                .toList();
     }
 
-    public List<Hobby> buscarPorCategoria(Long categoryId) {
-        return hobbyRepository.findByCategoryId(categoryId);
+    @Transactional
+    public Hobby buscarEntidadePorId(Long id) {
+        return hobbyRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Hobby não encontrado."));
     }
 }
--- src/main/java/dev/jamal/projetotcc/Service/UserService.java
+++ src/main/java/dev/jamal/projetotcc/Service/UserService.java
@@ -1,6 +1,9 @@
 package dev.jamal.projetotcc.Service;
 
+import dev.jamal.projetotcc.DTO.User.UserCreateRequestDTO;
+import dev.jamal.projetotcc.DTO.User.UserResponseDTO;
 import dev.jamal.projetotcc.Entities.User;
+import dev.jamal.projetotcc.Mapper.UserMapper;
 import dev.jamal.projetotcc.Repository.UserRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
@@ -14,19 +17,32 @@ import java.util.List;
 public class UserService {
 
     private final UserRepository userRepository;
+    private final UserMapper userMapper;
 
-    public User cadastrar(User user){
-        if (userRepository.existsByEmail(user.getEmail())){
+    public UserResponseDTO cadastrar(UserCreateRequestDTO dto){
+
+        if (userRepository.existsByEmail(dto.getEmail())){
             throw new RuntimeException("Email já cadastrado.");
         }
-        return userRepository.save(user);
+
+        User user = userMapper.toEntity(dto);
+        User salvo= userRepository.save(user);
+
+        return userMapper.toResponseDTO(salvo);
     }
 
-    public List<User> listarTodos(){
-        return userRepository.findAll();
+    @Transactional
+    public List<UserResponseDTO> listarTodos(){
+        return userRepository.findAll()
+                .stream()
+                .map(userMapper::toResponseDTO)
+                .toList();
     }
 
-    public User buscarPorId(Long id){
+    // Esse método pode existir para uso interno de outros services.
+    //  Mas controller não deve chamar ele diretamente.
+    @Transactional
+    public User buscarEntidadePorId(Long id){
         return userRepository.findById(id)
                 .orElseThrow(() -> new RuntimeException("Usuário não encontrado"));
     }
