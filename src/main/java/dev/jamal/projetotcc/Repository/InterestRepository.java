5affc37fd7bcd3504aa1b2b9fbe38021c739609f
btpjamal <jvbraga2608@gmail.com>
2026-04-29 14:15:33 -0300

    adiciona o método "findByNomeIgnoreCase", ele é necessário na classe de testes do package config

--- src/main/java/dev/jamal/projetotcc/Repository/InterestRepository.java
+++ src/main/java/dev/jamal/projetotcc/Repository/InterestRepository.java
@@ -4,6 +4,9 @@ import dev.jamal.projetotcc.Entities.Interest;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface InterestRepository extends JpaRepository<Interest, Long> {
+    Optional<Interest> findByNomeIgnoreCase(String nome);
 }
