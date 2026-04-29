42535cfc6e6d38b75ddcc2df088c9359a79f40d8
btpjamal <jvbraga2608@gmail.com>
2026-04-29 14:13:39 -0300

    adiciona o método "findByNomeIgnoreCase", ele é necessário nas classes de testes do package config

--- src/main/java/dev/jamal/projetotcc/Repository/HobbyCategoryRepository.java
+++ src/main/java/dev/jamal/projetotcc/Repository/HobbyCategoryRepository.java
@@ -7,7 +7,11 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface HobbyCategoryRepository extends JpaRepository<HobbyCategory, Long> {
+
+    Optional<HobbyCategory> findByNomeIgnoreCase(String nome);
+    boolean existsByNomeIgnoreCase(String nome);
 }
