0694abf579e7caf91596eff2e799c06713d6b8e5
btpjamal <jvbraga2608@gmail.com>
2026-04-29 14:14:38 -0300

    adiciona o método "existsByNomeIgnoreCase" e "findByNomeIgnoreCase", eles são necessários nas classes de testes do package config

--- src/main/java/dev/jamal/projetotcc/Repository/HobbyRepository.java
+++ src/main/java/dev/jamal/projetotcc/Repository/HobbyRepository.java
@@ -7,6 +7,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface HobbyRepository extends JpaRepository<Hobby, Long> {
@@ -17,4 +18,8 @@ public interface HobbyRepository extends JpaRepository<Hobby, Long> {
         JOIN FETCH h.category       
     """)
     List<Hobby> findAllWithCategory();
+
+    boolean existsByNomeIgnoreCase(String nome);
+
+    Optional<Hobby> findByNomeIgnoreCase(String nome);
 }
