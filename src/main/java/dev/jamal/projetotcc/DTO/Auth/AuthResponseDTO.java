fdb6faaa659efbe7b8122d5c4e0dab2578a06a4c
btpjamal <joaothexkronax@hotmail.com>
2026-05-04 17:01:47 -0300

    camada DTO para respostas de autenticação

---
+++ src/main/java/dev/jamal/projetotcc/DTO/Auth/AuthResponseDTO.java
@@ -0,0 +1,11 @@
+package dev.jamal.projetotcc.DTO.Auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class AuthResponseDTO {
+
+    private String token;
+}
